aba4ea24385a99b6cc7795df147f824f6eb2222a
Willem Liu <willem_liu@hotmail.com>
2020-06-19 14:31:29 +0200

    Develop branch

--- api/GetMessage/index.ts
+++ api/GetMessage/index.ts
@@ -7,7 +7,7 @@ const httpTrigger: AzureFunction = async function (
     context.log('HTTP trigger function processed a request.');
     const name = req.query.name || (req.body && req.body.name);
     const responseMessage = name
-        ? 'Hello, ' +
+        ? 'DEV Hello, ' +
           name +
           '. This HTTP triggered function executed successfully.'
         : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
