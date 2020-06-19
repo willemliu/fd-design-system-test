83bf66611e63430acf34d7c5d90818c680855b77
Willem Liu <willem_liu@hotmail.com>
2020-06-19 14:25:56 +0200

    Build function next to ts

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,48 +1,22 @@
-'use strict';
-var __awaiter =
-    (this && this.__awaiter) ||
-    function (thisArg, _arguments, P, generator) {
-        function adopt(value) {
-            return value instanceof P
-                ? value
-                : new P(function (resolve) {
-                      resolve(value);
-                  });
-        }
-        return new (P || (P = Promise))(function (resolve, reject) {
-            function fulfilled(value) {
-                try {
-                    step(generator.next(value));
-                } catch (e) {
-                    reject(e);
-                }
-            }
-            function rejected(value) {
-                try {
-                    step(generator['throw'](value));
-                } catch (e) {
-                    reject(e);
-                }
-            }
-            function step(result) {
-                result.done
-                    ? resolve(result.value)
-                    : adopt(result.value).then(fulfilled, rejected);
-            }
-            step(
-                (generator = generator.apply(thisArg, _arguments || [])).next()
-            );
-        });
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const httpTrigger = function (context, req) {
     return __awaiter(this, void 0, void 0, function* () {
         context.log('HTTP trigger function processed a request.');
         const name = req.query.name || (req.body && req.body.name);
         const responseMessage = name
             ? 'Hello, ' +
-              name +
-              '. This HTTP triggered function executed successfully.'
+                name +
+                '. This HTTP triggered function executed successfully.'
             : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
         context.res = {
             // status: 200, /* Defaults to 200 */
@@ -51,4 +25,3 @@ const httpTrigger = function (context, req) {
     });
 };
 exports.default = httpTrigger;
-//# sourceMappingURL=index.js.map
