93d049be29e2ade605730c2ba79cc4a5e2a977ff
Willem Liu <willem_liu@hotmail.com>
2020-06-22 13:39:25 +0200

    Removed DEV word

--- api/GetMessage/index.ts
+++ api/GetMessage/index.ts
@@ -11,7 +11,7 @@ const httpTrigger: AzureFunction = async function (
     const responseMessage = name
         ? `${
               process.env.ENVIRONMENT_DEV ?? ''
-          } DEV Hello, ${name}. This HTTP triggered function executed successfully.`
+          } Hello, ${name}. This HTTP triggered function executed successfully.`
         : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
 
     context.res = {
