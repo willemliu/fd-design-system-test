57917acd8c5e129cc02d0073c6beee8d8e76833a
Willem Liu <willem_liu@hotmail.com>
2020-06-22 12:46:30 +0200

    Trying out webpack for functions building

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const NODE_ENV = JSON.stringify(
+    process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+);
+const devtool = NODE_ENV == '"development"' ? 'source-map' : undefined;
+const isDev = NODE_ENV == '"development"';
+
+const webpackConfig = {
+    optimization: {
+        usedExports: true,
+        minimize: false,
+    },
+    entry: { 'GetMessage/index': './api/GetMessage/index.ts' },
+    output: {
+        path: path.join(__dirname, '/api/'),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: 'tsconfig.func.json',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
+
+module.exports = webpackConfig;
