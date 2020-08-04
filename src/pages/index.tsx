136b36bc3e9914e8123831d60593b64afaf251a0
Willem Liu <willem.liu@fdmediagroep.nl>
2020-08-04 14:09:12 +0200

    Updated dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -196,6 +196,7 @@ function Index() {
                 label="Detailhandel"
                 title="Den Haag zet druk op verhuurders om huurverlaging te slikken"
                 intro="Het Rijk sluit overheidsingrijpen niet uit om een akkoord tussen verhuurders en winkeliers af te dwingen, nu onderhandelingen moeizaam verlopen."
+                readingTime={7}
             />
 
             <hr />
